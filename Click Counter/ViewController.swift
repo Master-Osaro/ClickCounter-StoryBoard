b07337fa931dbb60cdc182381cb2ca898730e0f7
Osaro Iyoha <osaro@Osaros-Mac.local>
2018-08-09 01:46:40 +0100

    file Upload

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var count = 0
+    @IBOutlet var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("in Memory")
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    @IBAction func incrementCount() {
+        self.count += 1
+        self.label.text = "\(self.count)"
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("about to appear")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
 
+ 
---
+++ README.md
@@ -0,0 +1,5 @@
+# ClickCounter
+Simple swift3 iOS app with a button an a label that increments a digit by one
+
+Testing push...
+For this app, all additions and changes in the UI (Storyboard) was made programmatically via the ViewController
